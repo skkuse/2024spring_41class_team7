e5197af0b8bbecc64ac7d6cc50b6ee80ca2b314c
saem020402 <saem020402@g.skku.edu>
2024-05-21 10:58:24 +0900

    first frontend hs

--- Backend/app/services/carbon_calculator.py
+++ Backend/app/services/carbon_calculator.py
@@ -1,7 +1,7 @@
-def calculate_carbon_emission(t: float, nc: int, nm: int, uc: float, Pc: float, Pm: float, PUE: float) -> float:
+def calculate_carbon_emission(t: float, cores: int, memory: int, uc: float, Pc: float, Pm: float, PUE: float) -> float:
     """
     Calculate the carbon emission based on the given parameters.
     E = t × (nc × Pc × uc + nm × Pm) × PUE × 0.001
     """
-    E = t * (nc * Pc * uc + nm * Pm) * PUE * 0.001
+    E = t * (cores * Pc * uc + memory * Pm) * PUE * 0.001
     return E
---
+++ Frontend/src/pages/AdminPage.js
@@ -0,0 +1,12 @@
+function AdminPage() {
+    return (
+        <div>
+            <h2>Green Coders - Administrator</h2>
+            <table>
+
+            </table>
+        </div>
+    );
+}
+
+export default AdminPage;
