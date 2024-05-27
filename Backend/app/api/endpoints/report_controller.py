75c9f9d90458c21f83f4044c26ef6e292930e20d
jangseop <kgh010529@naver.com>
2024-05-28 07:29:51 +0900

    create new controller

---
+++ Backend/app/api/endpoints/report_controller.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter
+from Backend.app.models.requests import ReportRequest
+
+router = APIRouter()
+
+@router.get("/reports/")
+def create_report(new_report: ReportRequest):
+    return None
+
+@router.get("/reports/")
+def get_reports():
+    return None
+
+@router.get("/reports/{report_id]")
+def get_one_report(report_id: int):
+    return None
+
+@router.delete("/reports/{report_id}")
+def delete_report(report_id: int):
+    return None
+
+@router.delete("/reports/fixedCode/{fixed_code_id")
+def delete_report_by_fixed_code(fixed_code_id: int):
+    return None
