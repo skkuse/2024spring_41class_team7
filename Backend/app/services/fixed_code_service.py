3c878ad09d535b6180cbaa7963bc707851dd6a71
jangseop <kgh010529@naver.com>
2024-05-28 11:07:53 +0900

    create fixed-codes-controller test code

--- Backend/app/services/fixed_code_service.py
+++ Backend/app/services/fixed_code_service.py
@@ -1,2 +1,14 @@
 def create_fixed_code():
     return None
+
+def find_one():
+    return None
+
+def find_all():
+    return None
+
+def find_all_by_strategy():
+    return None
+
+def find_all_by_buggy_code():
+    return None
--- Backend/app/tests/test_fixed_code_controller.py
+++ Backend/app/tests/test_fixed_code_controller.py
@@ -1,7 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
-from Backend.app.models.response import  Response
-from Backend.app.main import app
+from ..main import app
+from ..models.response import Response, ListBody
 from unittest.mock import patch
 
 client = TestClient(app)
@@ -18,6 +18,7 @@ def mock_fixed_code_1():
         "fix_strategy_id": 1
     }
 
+@pytest.fixture
 def mock_fixed_code_2():
     return {
         "fixed_code_id": 2,
@@ -29,6 +30,7 @@ def mock_fixed_code_2():
         "fix_strategy_id": 2
     }
 
+@pytest.fixture
 def mock_fixed_code_3():
     return {
         "fixed_code_id": 3,
@@ -46,7 +48,7 @@ def mock_fixed_code_3():
 def test_create_fixed_code(mock_create_fixed_code, mock_fixed_code_1):
     mock_create_fixed_code.return_value = mock_fixed_code_1
 
-    response_raw = client.post("/fixedCodes/", json=mock_fixed_code_1)
+    response_raw = client.post("/api/fixedCodes/", json=mock_fixed_code_1)
     response = Response(**response_raw.json())
 
     assert response_raw.status_code == 200
@@ -58,20 +60,20 @@ def test_create_fixed_code(mock_create_fixed_code, mock_fixed_code_1):
 def test_get_fixed_codes(mock_find_all, mock_fixed_code_1, mock_fixed_code_2):
     mock_find_all.return_value = [mock_fixed_code_1, mock_fixed_code_2]
 
-    response_raw = client.get("/fixedCodes")
+    response_raw = client.get("/api/fixedCodes")
     response = Response(**response_raw.json())
 
     assert response_raw.status_code == 200
     assert response.status == "success"
-    assert response.onSuccess.nFixedCodes == 2
-    assert response.onSuccess.fixedCodes == [mock_fixed_code_1, mock_fixed_code_2]
+    assert ListBody(**response.onSuccess).nItems == 2
+    assert ListBody(**response.onSuccess).items == [mock_fixed_code_1, mock_fixed_code_2]
 
 # 단건 조회
 @patch("Backend.app.services.fixed_code_service.find_one")
 def test_get_one_fixed_code(mock_find_one, mock_fixed_code_1):
     mock_find_one.return_value = mock_fixed_code_1
 
-    response_raw = client.get("/fixedCodes/1")
+    response_raw = client.get("/api/fixedCodes/1")
     response = Response(**response_raw.json())
 
     assert response_raw.status_code == 200
@@ -83,7 +85,7 @@ def test_get_one_fixed_code(mock_find_one, mock_fixed_code_1):
 def test_get_one_fixed_code_not_found(mock_find_one, mock_fixed_code_1):
     mock_find_one.return_value = None
 
-    response_raw = client.get("/fixedCodes/1")
+    response_raw = client.get("/api/fixedCodes/1")
     response = Response(**response_raw.json())
 
     assert response_raw.status_code == 200
@@ -94,29 +96,29 @@ def test_get_one_fixed_code_not_found(mock_find_one, mock_fixed_code_1):
 
 
 # buggy code 별 검색
-@patch("Backend.app.services.fixed_code_service.find_one_by_buggy_code")
-def test_get_one_fixed_code_by_buggy_code(mock_find_one_by_buggy_code, mock_fixed_code_1, mock_fixed_code_2):
-    mock_find_one_by_buggy_code.return_value = [mock_fixed_code_1, mock_fixed_code_2]
+@patch("Backend.app.services.fixed_code_service.find_all_by_buggy_code")
+def test_get_one_fixed_code_by_buggy_code(mock_find_all_by_buggy_code, mock_fixed_code_1, mock_fixed_code_2):
+    mock_find_all_by_buggy_code.return_value = [mock_fixed_code_1, mock_fixed_code_2]
 
-    response_raw = client.get("/fixedCodes/buggyCode/1")
+    response_raw = client.get("/api/fixedCodes/buggyCode/1")
     response = Response(**response_raw.json())
 
     assert response_raw.status_code == 200
     assert response.status == "success"
-    assert response.onSuccess.nFixedCodes == 2
-    assert response.onSuccess.fixedCodes == [mock_fixed_code_1, mock_fixed_code_2]
+    assert ListBody(**response.onSuccess).nItems == 2
+    assert ListBody(**response.onSuccess).items == [mock_fixed_code_1, mock_fixed_code_2]
 
 # fix strategy 별 검색
-@patch("Backend.app.services.fixed_code_service.find_one_by_strategy")
-def test_get_one_fixed_code_by_strategy(mock_find_one_by_strategy, mock_fixed_code_1, mock_fixed_code_2):
-    mock_find_one_by_strategy.return_value = [mock_fixed_code_1, mock_fixed_code_3]
+@patch("Backend.app.services.fixed_code_service.find_all_by_strategy")
+def test_get_one_fixed_code_by_strategy(mock_find_all_by_strategy, mock_fixed_code_1, mock_fixed_code_3):
+    mock_find_all_by_strategy.return_value = [mock_fixed_code_1, mock_fixed_code_3]
 
-    response_raw = client.get("/fixedCodes/strategy/1")
+    response_raw = client.get("/api/fixedCodes/strategy/1")
     response = Response(**response_raw.json())
 
     assert response_raw.status_code == 200
     assert response.status == "success"
-    assert response.onSuccess.nFixedCodes == 2
-    assert response.onSuccess.fixedCodes == [mock_fixed_code_1, mock_fixed_code_3]
+    assert ListBody(**response.onSuccess).nItems == 2
+    assert ListBody(**response.onSuccess).items == [mock_fixed_code_1, mock_fixed_code_3]
 
 # 삭제 테스트 어떻게?
