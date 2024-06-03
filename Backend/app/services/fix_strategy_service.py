fe8581a48adffed8ff431bde589aee8907dfc751
jangseop <kgh010529@naver.com>
2024-06-04 01:44:21 +0900

    init service layer

---
+++ Backend/app/services/fix_strategy_service.py
@@ -0,0 +1,12 @@
+from ..models.requests import FixStrategyRequest
+from ..domain.domains import FixStrategy
+
+class FixStrategyService:
+    def create(self, new_fixStrategy: FixStrategy):
+        return None
+
+    def find_all(self):
+        return None
+
+    def find_one(self, id: int):
+        return None
