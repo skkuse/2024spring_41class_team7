17b98fd52596b06866d2f74cfc7bc54a03d50a07
jangseop <kgh010529@naver.com>
2024-06-04 01:45:44 +0900

    controller layers meet all unit testing :)

--- Backend/app/api/endpoints/buggy_codes_controller.py
+++ Backend/app/api/endpoints/buggy_codes_controller.py
@@ -1,18 +1,54 @@
-from fastapi import APIRouter
+from datetime import datetime
 
+from fastapi import APIRouter, FastAPI
+
+from ...domain.domains import BuggyCode
 from ...models.requests import BuggyCodeRequest
+from ...services.buggy_codes_service import BuggyCodesService
+from ...models.response import *
+from ...models.exceptions import *
+
+app = FastAPI()
 
 router = APIRouter()
 
-@router.post("/buggyCodes/")
-def create_buggy_code(new_buggy_code: BuggyCodeRequest):
-    return None
+SUCCESS = "success"
+FAIL = "fail"
 
-@router.get("/buggy_codes")
+@router.post("/")
+def req_create_buggy_code(new_buggy_code_req: BuggyCodeRequest):
+    new_buggy_code = BuggyCodesService.create(BuggyCode.create(new_buggy_code_req))
+
+    return Response(
+        status=SUCCESS,
+        onSuccess=BuggyCodeResponse.create(new_buggy_code),
+        onError=None
+    )
+@router.get("/")
 def get_buggy_codes():
-    return None
+    findBuggyCodes = BuggyCodesService.find_all()
 
-@router.get("/buggyCodes/{buggy_code_id}")
+    return Response(
+        status=SUCCESS,
+        onSuccess=ListBody(
+            nItems=len(findBuggyCodes),
+            items=map(lambda buggyCoodes: BuggyCodeResponse.create(buggyCoodes), findBuggyCodes)
+        ),
+        onError=None
+    )
+
+@router.get("/{buggy_code_id}")
 def get_one_buggy_code(buggy_code_id: int):
-    return None
+    try:
+        findBuggyCode = BuggyCodesService.find_one(id)
+        return Response(
+            status=SUCCESS,
+            onSuccess=BuggyCodeResponse.create(findBuggyCode),
+            onError=None)
+    except FileNotFoundError as e:
+        exception_body = handle_DataNotFoundException(buggy_code_id)
+        return Response(
+            status=FAIL,
+            onSuccess=None,
+            onError=exception_body)
 
--- Backend/app/domain/domains.py
+++ Backend/app/domain/domains.py
@@ -1,6 +1,10 @@
+import datetime
+
 from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Text
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+
+from ..models.requests import *
 
 Base = declarative_base()
 
@@ -16,6 +20,18 @@ class BuggyCode(Base):
     memory = Column(Integer, nullable=False)
     fixed_codes = relationship("FixedCode", back_populates="buggy_code")
 
+    @staticmethod
+    def create(buggy_code_req: BuggyCodeRequest):
+        return BuggyCode(
+            code_text=buggy_code_req.code_text,
+            created_at=datetime.datetime.now(),
+            emission_amount=buggy_code_req.emission_amount,
+            core_type=buggy_code_req.core_type,
+            core_model=buggy_code_req.core_model,
+            core_num=buggy_code_req.core_num,
+            memory=buggy_code_req.memory
+        )
+
 class FixedCode(Base):
     __tablename__ = 'fixed_code'
     fixed_code_id = Column(Integer, primary_key=True, autoincrement=True)
@@ -29,11 +45,26 @@ class FixedCode(Base):
     fix_strategy = relationship("FixStrategy", back_populates="fixed_codes")
     reports = relationship("Report", back_populates="fixed_code")
 
+    @staticmethod
+    def create(fixed_code_req: FixedCodeRequest):
+        return FixedCode(
+            buggy_part=fixed_code_req.buggy_part,
+            fixed_part=fixed_code_req.fixed_part,
+            reduced_amount=fixed_code_req.reduced_amount,
+            reduced_rate=fixed_code_req.reduced_rate,
+            buggy_code_id=fixed_code_req.buggy_code_id,
+            fix_strategy_id=fixed_code_req.fix_strategy_id
+        )
+
 class FixStrategy(Base):
     __tablename__ = 'fix_strategy'
     fix_strategy_id = Column(Integer, primary_key=True, autoincrement=True)
     description = Column(Text)
-    fix_codes = relationship("FixedCode", back_populates="fix_strategy")
+    fixed_codes = relationship("FixedCode", back_populates="fix_strategy")
+
+    @staticmethod
+    def create(fix_strategy_req: FixStrategyRequest):
+        return FixStrategy(description=fix_strategy_req.description)
 
 class Report(Base):
     __tablename__ = 'report'
@@ -42,4 +73,12 @@ class Report(Base):
     created_at = Column(DateTime, nullable=False)
     fixed_code_id = Column(Integer, ForeignKey('fixed_code.fixed_code_id'), nullable=False)
     fixed_code = relationship("FixedCode", back_populates="reports")
+
+    @staticmethod
+    def create(report_req: ReportRequest):
+        return Report(
+            description=report_req.description,
+            created_at=datetime.datetime.now(),
+            fixed_code_id=report_req.fixed_code_id
+        )
 
