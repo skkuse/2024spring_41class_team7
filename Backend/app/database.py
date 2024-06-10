008066d93237077edc08d3dec23eff5c2422089f
jangseop <kgh010529@naver.com>
2024-06-11 07:29:51 +0900

    edit db meta data setting

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -1,8 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = 'mysql+pymysql://root:310036@localhost:3306/green_coders_db'
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DB_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- Backend/app/main.py
+++ Backend/app/main.py
@@ -40,13 +40,13 @@ app.include_router(report_controller.router, prefix="/api/reports", tags=["repor
 
 load_dotenv()
 
-# DB_USER = os.getenv("DB_USER")
-# DB_PASSWORD = os.getenv("DB_PASSWORD")
-# DB_HOST = os.getenv("DB_HOST")
-# DB_PORT = os.getenv("DB_PORT")
-# DB_NAME = os.getenv("DB_NAME")
-#
-DB_URL = 'mysql+pymysql://root:310036@localhost:3306/green_coders_db'
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DB_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 engine = create_engine(DB_URL, pool_recycle=500)
 domains.Base.metadata.create_all(engine)
